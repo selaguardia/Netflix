05316d8a6eef20f3f5417a6d10aa03ca045a0ca6
selaguardia <selaguardia91@gmail.com>
2022-02-19 13:13:52 -0800

    navbar complete, for now

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons"
 import "./navbar.scss"
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null)
+  }
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Logonetflix.png/1600px-Logonetflix.png" alt="" />
@@ -18,7 +25,14 @@ const Navbar = () => {
           <span>KID</span>
           <Notifications className="icon" />
           <img src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg" alt="" />
-          <ArrowDropDown className="icon" />
+          <div className="profile">
+
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <img width="100%"src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg" alt="" />
     </div>
   )
 }
