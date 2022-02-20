d73805526a29c64ad969582b924c345825dfb792
selaguardia <selaguardia91@gmail.com>
2022-02-19 16:12:36 -0800

    list items hover and display video, and video info. does not autoplay

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,20 +1,52 @@
-import { Add, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined } from "@material-ui/icons"
-import "./listItem.scss"
+import {
+  Add,
+  PlayArrow,
+  ThumbDownAltOutlined,
+  ThumbUpAltOutlined,
+} from "@material-ui/icons";
+import { useState } from "react";
+import "./listItem.scss";
 
-function ListItem() {
+function ListItem({index}) {
+  const [isHovered, setIsHovered] = useState(false);
+  const trailer ="https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761"
+
   return (
-    <div className="listItem">
-      <img src="https://i.gadgets360cdn.com/large/witcher_blood_origin_1632571042826.jpeg" alt="" />
-      <div className="itemInfo">
-        <div className="icons">
-          <PlayArrow />
-          <Add />
-          <ThumbUpAltOutlined />
-          <ThumbDownAltOutlined />
-        </div>
-      </div>
+    <div
+      className="listItem"
+      style={{left: isHovered && index * 225 - 50 + index * 2.5}}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img
+        src="https://i.gadgets360cdn.com/large/witcher_blood_origin_1632571042826.jpeg"
+        alt=""
+      />
+      {isHovered && (
+        <>
+          <video src={trailer} autoPlay={true} loop />
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrow className="icon" />
+              <Add className="icon" />
+              <ThumbUpAltOutlined className="icon" />
+              <ThumbDownAltOutlined className="icon" />
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 14 mins</span>
+              <span className="limit">+16</span>
+              <span>2015</span>
+            </div>
+            <div className="desc">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+              dolorem dolores ea consequuntur iste!
+            </div>
+            <div className="genre">Sci-Fi</div>
+          </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default ListItem
+export default ListItem;
