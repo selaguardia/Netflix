978617cc0a8fa24ae0db5e1596d26f43786c74ee
selaguardia <selaguardia91@gmail.com>
2022-03-09 12:07:41 -0800

    adds login page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,6 +1,21 @@
+import { useRef, useState } from 'react'
 import './register.scss'
 
 export default function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const emailRef = useRef()
+  const passwordRef = useRef()
+
+  const handleStart = () => {
+    setEmail(emailRef.current.value)
+  }
+
+  const handleFinish = () => {
+    setPassword(passwordRef.current.value)
+  }
+
   return (
     <div className='register'>
       <div className="top">
@@ -13,10 +28,17 @@ export default function Register() {
         <h1>Unlimted movies, TV shows, and more.</h1>
         <h2>Watch anywhere. Cancel anytime.</h2>
         <p>Ready to watch? Enter your email to create or restart your membership.</p>
+      {!email ? (
         <div className="input">
-          <input type="email" placeholder='Email address' />
-          <button className="registerButton">Get Started</button>
+          <input type="email" placeholder='Email address' ref={emailRef}/>
+          <button className="registerButton" onClick={handleStart}>Get Started</button>
         </div>
+      ) : (
+        <form className="input">
+          <input type="password" placeholder='Password' ref={passwordRef}/>
+          <button className="registerButton" onClick={handleFinish}>Sign up</button>
+        </form>
+      )}
       </div>
     </div>
   )
