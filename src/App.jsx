a4faf2edd8e271040ee0149752228f2e461f509f
selaguardia <selaguardia91@gmail.com>
2022-06-11 15:03:50 -0700

    adds embedded trailer, and watch page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./app.scss"
-import Home from "./home/Home";
+import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
 
 const App = () => {
   return (
-    <Home />
+    // <Home />
+    <Watch />
   )
 }
 
