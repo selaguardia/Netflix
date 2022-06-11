9991f4c2fb3aa86dbe40d8f6b238420346340c7c
selaguardia <selaguardia91@gmail.com>
2022-06-11 11:14:24 -0700

    adds navbar, profile avatar, netflix logo, background image, nav styling, sass files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./app.scss"
+import Home from "./home/Home";
+
+const App = () => {
+  return (
+    <Home />
+  )
+}
+
+export default App
---
+++ src/home/Home.jsx
@@ -0,0 +1,66 @@
+import Navbar from '../components/navbar/Navbar';
+import './home.scss'
+import NetflixBackground from '../assets/netflix-background.jpeg'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Navbar />
+      <img src={NetflixBackground} width="100%" alt="Netflix Home Background Image" />
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, corporis dolores vitae est, quam incidunt fuga, inventore culpa dicta laborum obcaecati voluptatibus facilis a eius earum repellendus ipsum accusantium nobis!
+    </div>
+  )
+}
+
+export default Home
