24c7c7899d674bb3ee23e3c964c3bbe780700737
selaguardia <selaguardia91@gmail.com>
2023-02-11 10:29:08 -0800

    protects link if user not sign in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,20 @@
 import "./app.scss"
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 
 const App = () => {
+  const user = true;
   return (
     <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/series" element={<Home type="series" />} />
-      <Route path="/movies" element={<Home type="movies" />} />
-      <Route path="/watch" element={<Watch />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={!user ? <Login /> : <Navigate replace to={"/"} />} />
+      <Route path="/register" element={!user ? <Register /> : <Navigate replace to={"/"} />} />
+      <Route path="/" element={user ? <Home/> : <Register/>} />
+      <Route path="/series" element={user ? <Home type="series" /> : <Register/>} />
+      <Route path="/movies" element={user ? <Home type="movies" /> : <Register/>} />
+      <Route path="/watch" element={user ? <Watch /> : <Register/>} />
     </Routes>
   )
 }
